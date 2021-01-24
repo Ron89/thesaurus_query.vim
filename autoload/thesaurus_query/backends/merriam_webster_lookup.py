6a31f25a93b5a8b6da9586a2b61b45464cd7c766
root <aaron@flexile.co>
2021-01-24 22:35:55 +0000

    Fixed iOS need for explicit SSL (not required for later version of Python)

--- autoload/thesaurus_query/backends/merriam_webster_lookup.py
+++ autoload/thesaurus_query/backends/merriam_webster_lookup.py
@@ -47,7 +47,7 @@ def _dictionary_api_wrapper(target):
         return [-1, []]
     try:
         url = fixurl(u'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/{0}?key={1}'.format(target, api_key)).decode('ASCII') 
-        response = urlopen(url, timeout = time_out_choice).read()
+        response = urlopen(url, context=ssl.SSLContext(), timeout = time_out_choice).read()
         result_list = json.loads(response.decode('utf-8'))
     except HTTPError:
         return 1
